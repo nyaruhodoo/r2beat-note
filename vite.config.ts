2f8b818b1bbb8f037e4568ddab32454d67e0aca6
Nya <nyaruhodo9@gmail.com>
2026-08-17 19:30:07 +0800

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,6 +18,31 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'R2Beat Note',
+        short_name: 'R2BeatNote',
+        description: 'R2Beat Note Application',
+        theme_color: '#f8fafc',
+        background_color: '#f8fafc',
+        icons: [
+          {
+            src: 'pwa-192x192.png', // 请确保 public 目录下存在对应的图片文件
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png', // 请确保 public 目录下存在对应的图片文件
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
     }),
   ],
   resolve: {
